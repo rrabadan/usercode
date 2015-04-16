59c1f45f26a7036d793424e992b6546f327fe69e
Raul Rabadan <raul.iraq.rabadan.trejo@cern.ch>
2015-04-16 10:27:09 +0200

    printBadModLists_v1

---
+++ SiStripMonitorDigi/interface/SiStripBaselineValidator.h
@@ -0,0 +1,56 @@
+#ifndef SiStripMonitorDigi_SiStripBaselineValidator_h
+#define SiStripMonitorDigi_SiStripBaselineValidator_h
+
+// framework & common header files
+#include "FWCore/Utilities/interface/EDGetToken.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/Run.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+//DQM services
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/Common/interface/DetSetVectorNew.h"
+#include "DataFormats/SiStripDigi/interface/SiStripRawDigi.h"
+
+#include <iostream>
+#include <stdlib.h>
+
+
+#include <DQMServices/Core/interface/DQMEDAnalyzer.h>
+
+//using namespace reco;
+
+class DQMStore;
+class SiStripBaselineValidator : public DQMEDAnalyzer
+{
+ public:
+  explicit SiStripBaselineValidator(const edm::ParameterSet&);
+  virtual ~SiStripBaselineValidator();
+
+  virtual void beginJob();
+  virtual void endJob();  
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
+
+  private:
+
+  DQMStore *dbe;
+
+  MonitorElement *h1NumbadAPVsRes_;
+  MonitorElement *h1ADC_vs_strip_;
+
+  edm::InputTag srcProcessedRawDigi_;
+  edm::EDGetTokenT<edm::DetSetVector<SiStripRawDigi> > moduleRawDigiToken_;
+  std::string outputFile_;
+  bool createOutputFile_;
+
+};
+#endif
